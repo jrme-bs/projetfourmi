ff7174362cd17d270ff84fc9e65ede326e0de85d
sully <e22102349@etudiant.univ-brest.fr>
2024-12-03 15:19:24 +0100

    oubli de add bilan

---
+++ src/trace/Bilan.java
@@ -0,0 +1,74 @@
+package trace;
+
+public class Bilan {
+	private int nbOeuf = 0;
+	private int nbLarve = 0;
+	private int nbNymphe = 0;
+	private int nbAdulte = 0;
+	private int nbMort = 0;
+	
+	public Bilan() {
+	
+	}
+	
+	public void newDay() {
+		nbOeuf = 0;
+		nbLarve = 0;
+		nbNymphe = 0;
+		nbAdulte = 0;
+		nbMort = 0;
+	}
+	
+	public void afficheNbEtatFourmiliere() {
+		System.out.println("============================================");
+		System.out.println("Nombre d'oeuf : " + nbOeuf);
+		System.out.println("Nombre de larve : " + nbLarve);
+		System.out.println("Nombre de nymphe : " + nbNymphe);
+		System.out.println("Nombre de fourmis adulte : " + nbAdulte);
+		System.out.println("Nombre de fourmis morte : " + nbMort);
+		System.out.println("============================================");
+		
+		newDay();
+	}
+
+	public int getNbOeuf() {
+		return nbOeuf;
+	}
+
+	public void setNbOeuf(int nbOeuf) {
+		this.nbOeuf = nbOeuf;
+	}
+
+	public int getNbLarve() {
+		return nbLarve;
+	}
+
+	public void setNbLarve(int nbLarve) {
+		this.nbLarve = nbLarve;
+	}
+
+	public int getNbNymphe() {
+		return nbNymphe;
+	}
+
+	public void setNbNymphe(int nbNymphe) {
+		this.nbNymphe = nbNymphe;
+	}
+
+	public int getNbAdulte() {
+		return nbAdulte;
+	}
+
+	public void setNbAdulte(int nbAdulte) {
+		this.nbAdulte = nbAdulte;
+	}
+
+	public int getNbMort() {
+		return nbMort;
+	}
+
+	public void setNbMort(int nbMort) {
+		this.nbMort = nbMort;
+	}
+	
+}
